af868ac6eda15fdff3a8b0d92626dc8597437ac4
kemora_13conf <Abdomouak48@gmail.com>
2024-04-26 23:26:14 +0100

    responsiveness available now. x_0!

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,7 +1,9 @@
 const dark_mode_toggle = document.getElementById("toggle-darkmode");
+const navBtn = document.querySelector(".nav-btn");
+const navBar = document.querySelector(".nav-bar");
+const menu = navBar.querySelector(".menu");
 const menu_items = document.querySelectorAll(".menu-item");
 const menu_indicator = document.querySelector(".menu-indicator");
-
 
 // config theme from localStorage
 const theme = localStorage.getItem("theme") || "light";
@@ -9,6 +11,26 @@ if (theme === "dark") {
   document.documentElement.classList.add("dark");
   dark_mode_toggle.checked = true;
 }
+
+
+
+navBtn.addEventListener("click", () => {
+  if (navBtn.classList.contains("active")) {
+    navBtn.classList.remove("active");
+    menu.animate([{ height: "300px" }, { height: 0 }], {
+      duration: 300,
+      fill: "forwards",
+      easing: "ease-in-out",
+    });
+  } else {
+    navBtn.classList.add("active");
+    menu.animate([{ height: 0 }, { height: "300px" }], {
+      duration: 300,
+      fill: "forwards",
+      easing: "ease-in-out",
+    });
+  }
+});
 
 // Config the animation in the header menu
 menu_items.forEach((item) => {
@@ -23,7 +45,6 @@ menu_items.forEach((item) => {
     });
   });
 });
-
 
 // consfig dark mode toggle
 dark_mode_toggle.addEventListener("change", () => {
